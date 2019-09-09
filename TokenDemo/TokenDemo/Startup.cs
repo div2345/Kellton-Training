d5d1e1afe58d6e280b991909527bb16579f042e3
div2345 <31580386+div2345@users.noreply.github.com>
2019-09-09 19:01:12 +0530

    Add files via upload

---
+++ TokenDemo/TokenDemo/Startup.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+using Microsoft.OpenApi.Models;
+
+namespace TokenDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string securityKey = "this_is_our_supper_long_security_key_for_token_validation_$divyansh";
+            var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+              .AddJwtBearer(options =>
+              {
+                  options.TokenValidationParameters = new TokenValidationParameters
+                  {                      
+                      ValidateIssuer = true,
+                      ValidateAudience = true,
+                      ValidateIssuerSigningKey = true,
+             
+                      ValidIssuer = "divyansh",
+                      ValidAudience = "readers",
+                      IssuerSigningKey = symmetricSecurityKey
+                  };
+              });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);         
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseMvc();                            
+        }
+    }
+}
